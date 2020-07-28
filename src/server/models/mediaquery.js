31c835b04448a88ab38926c88d1624c274ad7519
William Chern <w.chern.9@gmail.com>
2020-07-28 12:06:41 -0700

    CANVAS-45 - Display available BruinCast content

--- src/server/models/mediaquery.js
+++ src/server/models/mediaquery.js
@@ -5,12 +5,33 @@ const { DB_DATABASE } = process.env;
 
 module.exports.getCastsByCourse = async courseLabel => {
   const castCollection = client.db(DB_DATABASE).collection('bruincastmedia');
-  castCollection.find({ classShortname: courseLabel }).toArray();
+  const toBeReturned = await castCollection
+    .find({ classShortname: courseLabel })
+    .toArray();
+  return toBeReturned;
 };
 
 module.exports.getCastCountByCourse = async courseLabel => {
   const arrayOfCasts = await this.getCastsByCourse(courseLabel);
   return arrayOfCasts.length;
+};
+
+module.exports.getCastsByTerm = async academicTerm => {
+  const recordsForTerm = await client
+    .db(DB_DATABASE)
+    .collection('bruincastmedia')
+    .find({ term: academicTerm })
+    .toArray();
+  return recordsForTerm;
+};
+
+module.exports.getAllBruinCastMedia = async function() {
+  const allRecords = await client
+    .db(DB_DATABASE)
+    .collection('bruincastmedia')
+    .find({})
+    .toArray();
+  return allRecords;
 };
 
 module.exports.getNotice = async () => {
