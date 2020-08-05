e6b6475d544b84ae38ae99be7fa9b96b902a5249
maxpluez <max@pop-os.localdomain>
2020-08-05 12:13:50 -0700

    CANVAS-42 - Fixed some linting issues

--- migrations/20200804193836-CreateCollectionsIfNotExist.js
+++ migrations/20200804193836-CreateCollectionsIfNotExist.js
@@ -1,5 +1,5 @@
 module.exports = {
-  async up(db, client) {
+  async up(db) {
     const collectionList = await db.listCollections().toArray();
     if (!collectionList.map(col => col.name).includes('crosslists')) {
       await db.createCollection('crosslists');
@@ -15,7 +15,7 @@ module.exports = {
     }
   },
 
-  async down(db, client) {
+  async down() {
     // TODO write the statements to rollback your migration (if possible)
     // Example:
     // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
