0827d422a196d7e5cd1dc0bf29ec25c489fc00a0
maxpluez <max@pop-os.localdomain>
2020-08-07 17:59:15 -0700

    CANVAS-40 - Added url generation for video reserves

--- src/server/services/BruincastServices.js
+++ src/server/services/BruincastServices.js
@@ -1,6 +1,3 @@
-const sha256 = require('crypto-js/sha256');
-const Base64 = require('crypto-js/enc-base64');
-
 const createDOMPurify = require('dompurify');
 const { JSDOM } = require('jsdom');
 const MediaQuery = require('../models/mediaquery');
@@ -128,45 +125,6 @@ class BruincastServices {
     const media = await MediaQuery.getCastsByTerm('bruincastmedia', term);
     const formattedArray = this.formatCastListings(media);
     return formattedArray;
-  }
-
-  static async generateMediaToken(stream, clientIP, secret, start, end) {
-    const { TOKEN_NAME } = process.env;
-    const params = [
-      `${TOKEN_NAME}starttime=${start}`,
-      `${TOKEN_NAME}endtime=${end}`,
-      secret,
-    ];
-    if (clientIP) {
-      params.push(clientIP);
-    }
-    params.sort();
-
-    let string4Hashing = `${stream}?`;
-    for (const entry of params) {
-      string4Hashing += `${entry}&`;
-    }
-    string4Hashing = string4Hashing.substr(0, string4Hashing.length - 1);
-    const hash = sha256(string4Hashing);
-    const base64Hash = Base64.stringify(hash)
-      .replace(/\+/g, '-')
-      .replace(/\//g, '_');
-    return base64Hash;
-  }
-
-  static async generateMediaURL(HOST, stream, clientIP, secret, start, end) {
-    const { TOKEN_NAME } = process.env;
-    const HOST_URL = `https://${HOST}:443/`;
-    const base64Hash = await this.generateMediaToken(
-      stream,
-      clientIP,
-      secret,
-      start,
-      end
-    );
-    const newStream = `redirect/${stream}`;
-    const playbackURL = `${HOST_URL}${newStream}?type=m3u8&${TOKEN_NAME}starttime=${start}&${TOKEN_NAME}endtime=${end}&${TOKEN_NAME}hash=${base64Hash}`;
-    return playbackURL;
   }
 }
 
