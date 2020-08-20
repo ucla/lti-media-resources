dcc3eddd479d3a7a7bbd98a4264e96a20383e565
William Chern <w.chern.9@gmail.com>
2020-08-20 12:25:10 -0700

    CANVAS-102 - Change bcast db field from classID to srs

--- src/server/jobs/update-bcast.js
+++ src/server/jobs/update-bcast.js
@@ -145,18 +145,18 @@ async function main() {
 
       // For each term and Class ID, get the associated media and update records
       for await (const course of courses) {
-        const currentClassID = course['srs #'];
+        const currentSRS = course['srs #'];
         const currentClassShortname = await registrar.getShortname(
           currentTerm,
-          currentClassID
+          currentSRS
         );
         logger.info(
-          `Updating records for ${currentClassShortname} (Class ID: ${currentClassID})`
+          `Updating records for ${currentClassShortname} (Class ID: ${currentSRS})`
         );
         let numCourseRecords = 0;
 
         // From the API, get media listings for given term and Class ID
-        await getMedia(formattedTerm, currentClassID, async function(
+        await getMedia(formattedTerm, currentSRS, async function(
           mediaResponse
         ) {
           const mediaEntries = JSON.parse(mediaResponse);
@@ -177,7 +177,7 @@ async function main() {
 
             mediaRecords.push({
               classShortname: currentClassShortname,
-              classID: currentClassID,
+              srs: currentSRS,
               term: currentTerm,
               date: mediaEntry['date for recording(s)'],
               week: weekNum,
@@ -195,7 +195,7 @@ async function main() {
             client,
             'bruincastmedia',
             currentTerm,
-            currentClassID,
+            currentSRS,
             mediaRecords,
             logger
           );
