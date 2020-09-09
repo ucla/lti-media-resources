652da3cecff3ceb8870cfbe79e607449780db7f1
maxpluez <max@pop-os.localdomain>
2020-09-08 18:58:07 -0700

    CANVAS-111 - All backend JSDocs

--- src/server/api/medias/musicres.js
+++ src/server/api/medias/musicres.js
@@ -5,6 +5,56 @@ const router = express.Router();
 const MusicresServices = require('../../services/MusicresServices');
 const CheckRoleServices = require('../../services/CheckRole');
 
+/**
+ * /:
+ *   get:
+ *     summary: Get all music reserves of current course
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved music reserves
+ *         schema:
+ *           type: array
+ *           items:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               performers:
+ *                 type: string
+ *               notes:
+ *                 type: string
+ *               label:
+ *                 type: string
+ *               labelCatalogNumber:
+ *                 type: string
+ *               callNumber:
+ *                 type: string
+ *               embedURL:
+ *                 type: string
+ *               items:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     trackTitle:
+ *                       type: string
+ *                     volume:
+ *                       type: string
+ *                     disc:
+ *                       type: string
+ *                     side:
+ *                       type: string
+ *                     trackNumber:
+ *                       type: string
+ *                     httpURL:
+ *                       type: string
+ *                     rtmpURL:
+ *                       type: string
+ *               workID:
+ *                 type: number
+ *       403:
+ *         description: Request rejected by user's role; access restriction
+ */
 router.get('/', (req, res) => {
   if (!CheckRoleServices.isUser(res.locals.context.roles)) {
     return res.status(403).send(new Error('Unauthorized role'));
@@ -16,6 +66,84 @@ router.get('/', (req, res) => {
   ).then(list => res.send(list));
 });
 
+/**
+ * /alllistings:
+ *   get:
+ *     summary: Get all Music Reserves listings, categorized by course shortname
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved and formatted listings
+ *         schema:
+ *           type: array
+ *           items:
+ *             type: object
+ *             properties:
+ *               _id:
+ *                 type: object
+ *                 properties:
+ *                   shortname:
+ *                     type: string
+ *                   term:
+ *                     type: string
+ *               subjectArea:
+ *                 type: string
+ *               listings:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     _id:
+ *                       type: ObjectId
+ *                     isVideo:
+ *                       type: boolean
+ *                     compaser:
+ *                       type: string
+ *                     title:
+ *                       type: string
+ *                     performers:
+ *                       type: string
+ *                     noteOne:
+ *                       type: string
+ *                     noteTwo:
+ *                       type: string
+ *                     label:
+ *                       type: string
+ *                     labelCatalogNumber:
+ *                       type: string
+ *                     callNumber:
+ *                       type: string
+ *                     items:
+ *                       type: array
+ *                       items:
+ *                         type: object
+ *                         properties:
+ *                           trackTitle:
+ *                             type: string
+ *                           volume:
+ *                             type: string
+ *                           disc:
+ *                             type: string
+ *                           side:
+ *                             type: string
+ *                           trackNumber:
+ *                             type: string
+ *                           httpURL:
+ *                             type: string
+ *                           rtmpURL:
+ *                             type: string
+ *                     workID:
+ *                       type: int
+ *                     term:
+ *                       type: string
+ *                     srs:
+ *                       type: string
+ *                     classShortname:
+ *                       type: string
+ *                     subjectArea:
+ *                       type: string
+ *       403:
+ *         description: Request rejected because user's role; forbidden.
+ */
 router.get('/alllistings', (req, res) => {
   if (!CheckRoleServices.isAdmin(res.locals.context.roles)) {
     return res.status(403).send(new Error('Unauthorized role'));
--- src/server/api/medias/videores.js
+++ src/server/api/medias/videores.js
@@ -5,6 +5,41 @@ const router = express.Router();
 const VideoresServices = require('../../services/VideoresServices');
 const CheckRoleServices = require('../../services/CheckRole');
 
+/**
+ * /:
+ *   get:
+ *     summary: Get all video reserves of current course
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved video reserves
+ *         schema:
+ *           type: array
+ *           items:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               startDate:
+ *                 type: string
+ *               stopDate:
+ *                 type: string
+ *               instructor:
+ *                 type: string
+ *               videoTitle:
+ *                 type: string
+ *               videoUrl:
+ *                 type: string
+ *               filename:
+ *                 type: string
+ *               subtitle:
+ *                 type: string
+ *               height:
+ *                 type: string
+ *               width:
+ *                 type: string
+ *       403:
+ *         description: Request rejected by user's role; access restriction
+ */
 router.get('/', (req, res) => {
   if (!CheckRoleServices.isUser(res.locals.context.roles)) {
     return res.status(403).send(new Error('Unauthorized role'));
@@ -16,6 +51,65 @@ router.get('/', (req, res) => {
   ).then(list => res.send(list));
 });
 
+/**
+ * /alllistings:
+ *   get:
+ *     summary: Get all Video Reserves listings, categorized by course shortname
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved and formatted listings
+ *         schema:
+ *           type: array
+ *           items:
+ *             type: object
+ *             properties:
+ *               _id:
+ *                 type: object
+ *                 properties:
+ *                   shortname:
+ *                     type: string
+ *                   term:
+ *                     type: string
+ *               subjectArea:
+ *                 type: string
+ *               listings:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     _id:
+ *                       type: ObjectId
+ *                     term:
+ *                       type: string
+ *                     srs:
+ *                       type: string
+ *                     startDate:
+ *                       type: string
+ *                     stopDate:
+ *                       type: string
+ *                     title:
+ *                       type: string
+ *                     instructor:
+ *                       type: string
+ *                     videoTitle:
+ *                       type: string
+ *                     videoUrl:
+ *                       type: string
+ *                     filename:
+ *                       type: string
+ *                     subtitle:
+ *                       type: string
+ *                     height:
+ *                       type: string
+ *                     width:
+ *                       type: string
+ *                     classShortname:
+ *                       type: string
+ *                     subjectArea:
+ *                       type: string
+ *       403:
+ *         description: Request rejected because user's role; forbidden.
+ */
 router.get('/alllistings', (req, res) => {
   if (!CheckRoleServices.isAdmin(res.locals.context.roles)) {
     return res.status(403).send(new Error('Unauthorized role'));
