faea81bf764ab3c62b4af52c77eeb6cee98f9797
maxpluez <pipi-max@hotmail.com>
2020-09-11 01:13:09 -0700

    CANVAS-43 - Removed media gallery (#72)
    Co-authored-by: maxpluez <max@pop-os.localdomain>

--- constants.js
+++ constants.js
@@ -2,8 +2,7 @@ module.exports.TABS = {
   BRUINCAST: 0,
   VIDEO_RESERVES: 1,
   DIGITAL_AUDIO_RESERVES: 2,
-  MEDIA_GALLERY: 3,
-  ADMIN_PANEL: 4,
+  ADMIN_PANEL: 3,
 };
 
 module.exports.ADMIN_PANEL_TABS = {
@@ -17,7 +16,6 @@ module.exports.MEDIA_TYPE = {
   BRUINCAST: 0,
   VIDEO_RESERVES: 1,
   DIGITAL_AUDIO_RESERVES: 2,
-  MEDIA_GALLERY: 3,
 };
 
 module.exports.mediaTypeMap = new Map([
@@ -42,30 +40,21 @@ module.exports.mediaTypeMap = new Map([
       string: 'Digital Audio Reserves',
     },
   ],
-  [
-    module.exports.MEDIA_TYPE.MEDIA_GALLERY,
-    {
-      api: '',
-      string: 'Media Gallery',
-    },
-  ],
 ]);
 
 module.exports.COLLECTION_TYPE = {
   BRUINCAST: 0,
   VIDEO_RESERVES: 1,
   DIGITAL_AUDIO_RESERVES: 2,
-  MEDIA_GALLERY: 3,
-  CROSSLISTS: 4,
-  PLAYBACKS: 5,
-  NOTICE: 6,
+  CROSSLISTS: 3,
+  PLAYBACKS: 4,
+  NOTICE: 5,
 };
 
 module.exports.collectionMap = new Map([
   [module.exports.COLLECTION_TYPE.BRUINCAST, 'bruincastmedia'],
   [module.exports.COLLECTION_TYPE.VIDEO_RESERVES, 'videoreserves'],
   [module.exports.COLLECTION_TYPE.DIGITAL_AUDIO_RESERVES, 'musicreserves'],
-  [module.exports.COLLECTION_TYPE.MEDIA_GALLERY, 'mediagallery'],
   [module.exports.COLLECTION_TYPE.CROSSLISTS, 'crosslists'],
   [module.exports.COLLECTION_TYPE.PLAYBACKS, 'playbacks'],
   [module.exports.COLLECTION_TYPE.NOTICE, 'notice'],
