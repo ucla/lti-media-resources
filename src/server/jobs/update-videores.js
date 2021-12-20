f4f9843ecb85faaef1d1db9d817fd611d73756ab
bkoraganti <88458606+bkoraganti@users.noreply.github.com>
2021-12-20 15:41:48 -0800

    Update update-videores.js

--- src/server/jobs/update-videores.js
+++ src/server/jobs/update-videores.js
@@ -1,5 +1,4 @@
-const FtpClient = require('ftp');
-//const sftpClient = require('ssh2-sftp-client');
+const sftpClient = require('ssh2-sftp-client');
 require('dotenv').config();
 const RegistrarService = require('../services/registrar');
 const UpdateVideoResServices = require('../services/UpdateReserveServices');
@@ -13,32 +12,34 @@ const client = require('../models/db');
 async function main() {
   const logger = await LogServices.createLogger('update-videores');
   const dbclient = await client.connect(process.env.DB_URL);
+  logger.info(`***********************`);
   logger.info(`Connected to database`);
 
   let result = [];
   const srsShortnameMap = [];
   
-  const c = new FtpClient();
 // sftp connectivity
-  //const c = new sftpClient();
-  c.on('ready', function () {
-    c.get(process.env.SECRET_VIDEORES_FILEPATH, function (err, stream) {
-      if (err) throw err;
-      stream.once('close', function () {
-        c.end();
-      });
-      let str = '';
-      stream.on('data', (data) => {
-        str += data.toString();
-      });
-      stream.on('end', async () => {
-        result = UpdateVideoResServices.readStrIntoFields(str, logger);
-      });
-    });
-  });
-  c.once('end', async function () {
+  const sftpClient = new sftpClient();
+    
+  sftpClient.connect({
+    host: process.env.SECRET_VIDEORES_FTP_HOST,
+    user: process.env.SECRET_VIDEORES_FTP_USERNAME,
+    password: process.env.SECRET_VIDEORES_FTP_PWD
+  }).then(() => {
+    console.log('reading file ....', process.env.SECRET_VIDEORES_FILEPATH);
+    return sftpClient.get(process.env.SECRET_VIDEORES_FILEPATH);
+  })
+  .then(async (data) => {
+    let str = '';
+    str += data.toString();
+    result = UpdateVideoResServices.readStrIntoFields(str, logger);
+    data.results = result;
+    return data;
+  })
+  .then(async (data) => {
+    console.log('processing the results... ')
     // Create an array of courses and get shortname for each course
-    for (const media of result) {
+    for (const media of data.results) {
       const mappedPair = srsShortnameMap.filter(
         (pair) => pair.srs === media.srs && pair.term === media.term
       );
@@ -46,7 +47,7 @@ async function main() {
         media.classShortname = mappedPair[0].classShortname;
         media.subjectArea = mappedPair[0].subjectArea;
       } else {
-        const { shortname, subjectArea } = await RegistrarService.getShortname(
+        const { shortname, subjectArea } = RegistrarService.getShortname(
           media.term,
           media.srs
         );
@@ -67,8 +68,9 @@ async function main() {
 
     // For each course, update its records
     let totalNumDiff = 0;
+    console.log('iterate srsShortnameMap results.... ')
     for (const srsPair of srsShortnameMap) {
-      totalNumDiff += await UpdateVideoResServices.updateRecordsForClass(
+      totalNumDiff += UpdateVideoResServices.updateRecordsForClass(
         dbclient,
         collectionMap.get(COLLECTION_TYPE.VIDEO_RESERVES),
         srsPair.term,
@@ -79,6 +81,7 @@ async function main() {
         logger
       );
     }
+    console.log('totalNumDiff length .... ', totalNumDiff.length);
     const totalNumEntries = result.length;
     if (totalNumDiff < 0) {
       logger.info(
@@ -89,15 +92,15 @@ async function main() {
         `Done. Processed ${totalNumEntries} entries with ${totalNumDiff} insertion(s).`
       );
     }
+
     dbclient.close();
     process.exit(0);
+  })
+  .catch(err => {
+    console.error(err.message);
+    sftpClient.end();
   });
-  // Connect to localhost:21 as anonymous
-  c.connect({
-    host: process.env.SECRET_VIDEORES_FTP_HOST,
-    user: process.env.SECRET_VIDEORES_FTP_USERNAME,
-    password: process.env.SECRET_VIDEORES_FTP_PWD,
-  });
+  console.log('at the end  .... ')
 }
 
 main();
