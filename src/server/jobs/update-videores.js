0238a3d4e6f5b3271c459467dab246e8d2dbf657
bkoraganti <88458606+bkoraganti@users.noreply.github.com>
2021-12-21 12:25:09 -0800

    Update update-videores.js

--- src/server/jobs/update-videores.js
+++ src/server/jobs/update-videores.js
@@ -31,12 +31,11 @@ async function main() {
   .then(async (data) => {
     let str = '';
     str += data.toString();
-    result = UpdateVideoResServices.readStrIntoFields(str, logger);
+    result = await UpdateVideoResServices.readStrIntoFields(str, logger);
     data.results = result;
     return data;
   })
   .then(async (data) => {
-    logger.info('processing the results... ')
     // Create an array of courses and get shortname for each course
     for (const media of data.results) {
       const mappedPair = srsShortnameMap.filter(
@@ -46,7 +45,7 @@ async function main() {
         media.classShortname = mappedPair[0].classShortname;
         media.subjectArea = mappedPair[0].subjectArea;
       } else {
-        const { shortname, subjectArea } = RegistrarService.getShortname(
+        const { shortname, subjectArea } = await RegistrarService.getShortname(
           media.term,
           media.srs
         );
@@ -67,9 +66,8 @@ async function main() {
 
     // For each course, update its records
     let totalNumDiff = 0;
-    logger.info('iterate srsShortnameMap results.... ')
     for (const srsPair of srsShortnameMap) {
-      totalNumDiff += UpdateVideoResServices.updateRecordsForClass(
+      totalNumDiff += await UpdateVideoResServices.updateRecordsForClass(
         dbclient,
         collectionMap.get(COLLECTION_TYPE.VIDEO_RESERVES),
         srsPair.term,
@@ -80,7 +78,6 @@ async function main() {
         logger
       );
     }
-    logger.info('totalNumDiff length .... ', totalNumDiff.length);
     const totalNumEntries = result.length;
     if (totalNumDiff < 0) {
       logger.info(
