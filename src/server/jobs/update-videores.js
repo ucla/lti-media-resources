7e15719045786e6d19d84de02886faf55a35a9d0
Rex Lorenzo <rexlorenzo@gmail.com>
2021-12-21 18:03:31 -0800

    Fixed linting issues and failing test

--- src/server/jobs/update-videores.js
+++ src/server/jobs/update-videores.js
@@ -21,82 +21,80 @@ async function main() {
   c.connect({
     host: process.env.SECRET_VIDEORES_FTP_HOST,
     user: process.env.SECRET_VIDEORES_FTP_USERNAME,
-    password: process.env.SECRET_VIDEORES_FTP_PWD
-  }).then(() => {
-    
-    logger.info('reading file ....', process.env.SECRET_VIDEORES_FILEPATH);
-    return c.get(process.env.SECRET_VIDEORES_FILEPATH);
+    password: process.env.SECRET_VIDEORES_FTP_PWD,
   })
-  .then(async (data) => {
-    
-    let str = '';
-    str += data.toString();
-    result = await UpdateVideoResServices.readStrIntoFields(str, logger);
-    data.results = result;
-    return data;
-  })
-  .then(async (data) => {
-    
-    // Create an array of courses and get shortname for each course
-    for (const media of data.results) {
-      const mappedPair = srsShortnameMap.filter(
-        (pair) => pair.srs === media.srs && pair.term === media.term
-      );
-      if (mappedPair && mappedPair.length !== 0) {
-        media.classShortname = mappedPair[0].classShortname;
-        media.subjectArea = mappedPair[0].subjectArea;
-      } else {
-        const { shortname, subjectArea } = await RegistrarService.getShortname(
-          media.term,
-          media.srs
+    .then(() => {
+      logger.info('reading file ....', process.env.SECRET_VIDEORES_FILEPATH);
+      return c.get(process.env.SECRET_VIDEORES_FILEPATH);
+    })
+    .then(async (data) => {
+      let str = '';
+      str += data.toString();
+      result = await UpdateVideoResServices.readStrIntoFields(str, logger);
+      data.results = result;
+      return data;
+    })
+    .then(async (data) => {
+      // Create an array of courses and get shortname for each course
+      for (const media of data.results) {
+        const mappedPair = srsShortnameMap.filter(
+          (pair) => pair.srs === media.srs && pair.term === media.term
         );
+        if (mappedPair && mappedPair.length !== 0) {
+          media.classShortname = mappedPair[0].classShortname;
+          media.subjectArea = mappedPair[0].subjectArea;
+        } else {
+          const {
+            shortname,
+            subjectArea,
+          } = await RegistrarService.getShortname(media.term, media.srs);
 
-        if (!shortname) {
-          logger.warn(`${media.term}-${media.srs} does not have shortname`);
+          if (!shortname) {
+            logger.warn(`${media.term}-${media.srs} does not have shortname`);
+          }
+          media.classShortname = shortname;
+          media.subjectArea = subjectArea;
+          srsShortnameMap.push({
+            term: media.term,
+            srs: media.srs,
+            classShortname: shortname,
+            subjectArea,
+          });
         }
-        media.classShortname = shortname;
-        media.subjectArea = subjectArea;
-        srsShortnameMap.push({
-          term: media.term,
-          srs: media.srs,
-          classShortname: shortname,
-          subjectArea,
-        });
       }
-    }
 
-    // For each course, update its records
-    let totalNumDiff = 0;
-    for (const srsPair of srsShortnameMap) {
-      totalNumDiff += await UpdateVideoResServices.updateRecordsForClass(
-        dbclient,
-        collectionMap.get(COLLECTION_TYPE.VIDEO_RESERVES),
-        srsPair.term,
-        srsPair.srs,
-        result.filter(
-          (media) => media.srs === srsPair.srs && media.term === srsPair.term
-        ),
-        logger
-      );
-    }
-    const totalNumEntries = result.length;
-    if (totalNumDiff < 0) {
-      logger.info(
-        `Done. Processed ${totalNumEntries} entries with ${-totalNumDiff} deletion(s).`
-      );
-    } else {
-      logger.info(
-        `Done. Processed ${totalNumEntries} entries with ${totalNumDiff} insertion(s).`
-      );
-    }
+      // For each course, update its records
+      let totalNumDiff = 0;
+      for (const srsPair of srsShortnameMap) {
+        totalNumDiff += await UpdateVideoResServices.updateRecordsForClass(
+          dbclient,
+          collectionMap.get(COLLECTION_TYPE.VIDEO_RESERVES),
+          srsPair.term,
+          srsPair.srs,
+          result.filter(
+            (media) => media.srs === srsPair.srs && media.term === srsPair.term
+          ),
+          logger
+        );
+      }
+      const totalNumEntries = result.length;
+      if (totalNumDiff < 0) {
+        logger.info(
+          `Done. Processed ${totalNumEntries} entries with ${-totalNumDiff} deletion(s).`
+        );
+      } else {
+        logger.info(
+          `Done. Processed ${totalNumEntries} entries with ${totalNumDiff} insertion(s).`
+        );
+      }
 
-    dbclient.close();
-    process.exit(0);
-  })
-  .catch(err => {
-    logger.error(err.message);
-    c.end();
-  });
+      dbclient.close();
+      process.exit(0);
+    })
+    .catch((err) => {
+      logger.error(err.message);
+      c.end();
+    });
 }
 
 main();
